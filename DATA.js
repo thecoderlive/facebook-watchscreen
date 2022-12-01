c9d4ee453e007cd283b4121cf3c5021b01d64117
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-01 14:48:09 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,50 @@
+export const item = {
+    "id": "416ed259-e712-4806-9064-6a0ccc302d65",
+    "watch_list": [
+        {
+            "id": "4ccbfb99-c3a3-4511-b53d-8fed52ddc39b",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png",
+            "user_name": "letter",
+            "hash_tag": "world",
+            "video_thumbnail": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/65.png",
+            "like_counts": "own",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+        },
+        {
+            "id": "f1b49c07-3250-41a4-ba5e-32664c284dbc",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png",
+            "user_name": "window",
+            "hash_tag": "nation",
+            "video_thumbnail": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png",
+            "like_counts": "life",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+        },
+        {
+            "id": "ee2e37be-eb43-4915-90dc-e76e861970fc",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png",
+            "user_name": "position",
+            "hash_tag": "outside",
+            "video_thumbnail": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png",
+            "like_counts": "behind",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+        },
+        {
+            "id": "2ddf0a4f-54d5-491b-a7d9-79c5d56095c2",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png",
+            "user_name": "special",
+            "hash_tag": "lawyer",
+            "video_thumbnail": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png",
+            "like_counts": "there",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png"
+        },
+        {
+            "id": "f4858190-ed32-4090-8d9b-e7e7211b9b78",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png",
+            "user_name": "after",
+            "hash_tag": "red",
+            "video_thumbnail": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/93.png",
+            "like_counts": "hard",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+        }
+    ]
+}
